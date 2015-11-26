26bbe213ce7c9362dad24f87bb5f7ebdde8ff908
unknown <tchen@DSGDYN7Q52.prod.quest.corp>
2015-11-26 17:21:59 +0800

    更新左侧菜单

--- src/webui/src/admin/app/layout/sidebar-menu/sidebar-menu.js
+++ src/webui/src/admin/app/layout/sidebar-menu/sidebar-menu.js
@@ -5,72 +5,68 @@ angular.module('app.admin.layout')
             transclude: true,
             replace: true,
             templateUrl: 'app/layout/sidebar-menu/sidebar-menu.tpl.html',
-            controller: ['$rootScope', '$scope', '$location', function($rootScope, $scope, $location) {
-            	$scope.tabs = [
-            		{
-            			"title": "首页",
-            			"key": "home",
-            			"icon": "fa-home",
-            			"active": false,
-            			"expanded": false,
-            			"childs": []
-            		},
-            		{	
-            			"title": "内容管理",
-            			"key": "article, category",
-            			"icon": "",
-            			"active": false,
-            			"expanded": false,
-            			"childs": [
-            				{
-            					"title": "类别管理",
-            					"key": "category",
-            					"icon": "",
-            					"active": false
-            				},
-            				{
-            					"title": "文章管理",
-            					"key": "article",
-            					"icon": "",
-            					"active": false
-            				}
-            			]
-            		},
-            	];
-            	
-            	$scope.initTabs = function(){
-            		var path = $location.url();
-	            	$scope.tabs = _.each($scope.tabs, function(tab){
-	            		if(path.indexOf(tab.key) > -1){
-	            			tab.active = true;
-	            		}else{
-	            			tab.active = false;
-	            		}
-	            		return _.each(tab.childs, function(child){
-	            			if(path.indexOf(child.key) > -1){
-	            				tab.active = true;
-	            				child.active = true;
-	            			}
-	            			return tab;
-	            		});
-	            	});
-            	};
+            controller: ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
+                $scope.tabs = [
+                    {
+                        "title": "首页",
+                        "route": "home",
+                        "icon": "fa-home",
+                        "active": false,
+                        "expanded": false,
+                        "childs": []
+                    },
+                    {
+                        "title": "内容管理",
+                        "route": "article, category",
+                        "icon": "",
+                        "active": false,
+                        "expanded": false,
+                        "childs": [
+                            {
+                                "title": "类别管理",
+                                "route": "category",
+                                "icon": "",
+                                "active": false
+                            },
+                            {
+                                "title": "文章管理",
+                                "route": "article",
+                                "icon": "",
+                                "active": false
+                            }
+                        ]
+                    }
+                ];
 
-            	$scope.expandTab = function(selectedTab){
-            		$scope.tabs = _.each($scope.tabs, function(tab){
-            			if(selectedTab == tab) {
-            				tab.expanded = !tab.expanded;
-            				tab.active = !tab.active;
-            			} else {
-            				tab.active = false;
-            			}
-            			return tab;
-            		});
-            	};
+                $scope.initTabs = function () {
+                    var path = $location.url();
+                    $scope.tabs = _.each($scope.tabs, function (tab) {
+                        tab.active = path.indexOf(tab.route) > -1 ? true : false;
+                        return _.each(tab.childs, function (child) {
+                            if (path.indexOf(child.route) > -1) {
+                                tab.active = true;
+                                child.active = true;
+                            }
+                            return tab;
+                        });
+                    });
+                };
 
-            	$rootScope.$on('$stateChangeSuccess', function(){
-            		$scope.initTabs();
-            	});
+                $scope.expandTab = function (selectedTab) {
+                    $scope.tabs = _.each($scope.tabs, function (tab) {
+                        if (selectedTab == tab) {
+                            tab.expanded = !tab.expanded;
+                            tab.active = !tab.active;
+                        } else {
+                            tab.active = false;
+                        }
+                        return tab;
+                    });
+                };
+
+                $rootScope.$on('$stateChangeSuccess', function () {
+                    $scope.initTabs();
+                });
             }]
         }
     });
