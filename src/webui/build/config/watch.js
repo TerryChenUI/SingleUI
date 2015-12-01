0ca6049aece51e47614adefd16adb9f0fbd012f4
unknown <tchen@DSGDYN7Q52.prod.quest.corp>
2015-12-01 13:51:56 +0800

    更新配置

--- src/webui/build/config/watch.js
+++ src/webui/build/config/watch.js
@@ -4,10 +4,10 @@ var gulp = require("gulp"),
     Server = require('karma').Server;
 
 gulp.task('watch', [], function (cb) {
-    gulp.watch("src/index.html", ["dev_index"]);
-    gulp.watch("src/admin/index.html", ["dev_admin_index"]);
-    gulp.watch("src/admin/login.html", ["dev_login"]);
-    gulp.watch(['src/**/*.html'], ["copy:html"]);
+    gulp.watch("src/index.html", ["inject:dev_index"]);
+    gulp.watch("src/admin/index.html", ["inject:dev_admin_index"]);
+    gulp.watch("src/admin/login.html", ["inject:dev_login"]);
+    gulp.watch(['src/**/*.tpl.html'], ["copy:html"]);
     gulp.watch(['src/**/*.scss'], ["renderSass"]);
     gulp.watch(['src/**/*.js','!src/**/*.spec.js','!src/**/*.scenario.js'], ["copy:js"]);
     // gulp.watch(['./src/**/*.spec.js'], ['karma:unit']);
